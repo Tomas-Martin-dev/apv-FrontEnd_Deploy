1e7969b04f22f0b42ff7a1ba8bc8e871f45b957b
Tomas Martin <martintomas898@gmail.com>
2025-05-14 00:49:27 -0300

    correcion de color text

--- src/componentes/Alerta.jsx
+++ src/componentes/Alerta.jsx
@@ -1,7 +1,7 @@
 const Alerta = ({alerta, className}) => {
     return (
     <div
-        className={`${alerta.error ? "from-red-400 to-red-600" : alerta.alert ? "from-amber-200 to-amber-200" : alerta.msgg ? "bg-slate-100 border border-slate-100 font-semibold text-neutral-500" : "from-indigo-400 to-indigo-600"} 
+        className={`${alerta.error ? "from-red-400 to-red-600" : alerta.alert ? "from-amber-200 to-amber-200" : alerta.msgg ? "bg-slate-100 border border-slate-100 font-semibold !text-neutral-500" : "from-indigo-400 to-indigo-600"} 
         bg-gradient-to-r p-3 rounded-xl uppercase text-white font-bold text-center text-sm ${className}`}
     >
         {alerta.msg}
